f279ac76c8302faefac818e2c62297642db23f81
EmrahGuvendik <EmrahGuvendik@EG>
2022-09-18 00:39:56 +0300

    Sysout Print

---
+++ Engin-Demirog-Java-Dersleri-2/src/Intro/Main.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Intro;
+
+/**
+ * @author EmrahGuvendik
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Kod satırı noktalı virgül (;) ile biter.
+		System.out.println("Merhaba Java ");
+		
+		// Aşağıdaki gibi bir yapıda da kod sorunsuz çalışacaktır fakat dağınık bir kod yazımı olduğundan 
+		// tercih edilmesi pek mantıklı değil.
+		System
+		.out
+		.println
+		(
+				"Merhaba Java2"
+				)
+		;
+		
+		
+		
+
+	}
+
+}
