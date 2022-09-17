441b97a3bec178bcf111bca914750d23ccbc6f12
EmrahGuvendik <EmrahGuvendik@EG>
2022-09-18 01:04:47 +0300

    Değişken Atamaları

---
+++ Variables/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Değişkenler !
+		// int (integer) tam sayı anlamına gelmektedir ve sayıları ifade eder
+		// Bir sayıyı değişkene atamamız gerekiyor ise int kullanmalıyız.
+		int ogrenciSayisi = 20;
+		
+		// String metinsel verileri ifade eder. Bir metni değişkene atmaamız gerekirse String kullanmalıyız.
+		String mesaj = "Öğrenci Sayısı : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
